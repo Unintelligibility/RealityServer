9a6cacfdd2206e56c1b9147ad502db2b570d4dc9
151250093 <151250093@smail.nju.edu.cn>
2018-01-28 23:17:54 +0800

    get news from database

--- RealityServer/__init__.py
+++ RealityServer/__init__.py
@@ -4,11 +4,13 @@ define app and routes
 from flask import Flask, Blueprint
 from flask_restful import Resource, Api, url_for
 from RealityServer.resourses.news import News
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 api = Api(app)
-
 api.add_resource(News, '/read')
+
+# mongo = PyMongo(app)
 
 if __name__ == '__main__':
     app.run()
--- RealityServer/resourses/news.py
+++ RealityServer/resourses/news.py
@@ -1,9 +1,21 @@
 from flask_restful import Resource
+from pymongo import MongoClient
+from bson.json_util import loads, dumps
+from pprint import pprint
+import json
 
 
 class News(Resource):
+    def __init__(self):
+        client = MongoClient('mongodb://118.89.178.181/')
+        db = client.reality
+        self.news = db.news
+
     def get(self):
-        pass
+        res = {i: x for i, x in enumerate(self.news.find())}
+        # pprint(res)
+        res = json.loads(dumps(res))
+        return res
 
     def post(self):
         pass
