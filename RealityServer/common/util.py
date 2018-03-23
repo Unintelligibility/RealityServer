9be1f854ec617350327041b1ea6a74e69b6dab47
Linwenye <419766878@qq.com>
2018-03-23 20:14:29 +0800

    add check for post

--- RealityServer/common/util.py
+++ RealityServer/common/util.py
@@ -7,7 +7,7 @@ def oid_transform_search_dic(my_dict):
     for x, y in my_dict.items():
         y['_id'] = str(y['_id'])
         y['fake'] = 0
-        if y['title'] == '草莓还能这样做？太太太好吃了！':
+        if y['title'] == '詹皇反对NBA增设季后赛附加赛：太奇怪了':
             y['fake'] = 1
         y['clickbait'] = 0
 
